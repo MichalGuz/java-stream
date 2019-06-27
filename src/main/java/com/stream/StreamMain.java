c572ea7d875abed43570535156dec2c07bd29316
MichalGuz <michalguz@gmail.com>
2019-06-27 23:29:26 +0200

    Create new object of PrintSomething class and execute method printSomething() for printing a text.

--- src/main/java/com/stream/StreamMain.java
+++ src/main/java/com/stream/StreamMain.java
@@ -1,7 +1,12 @@
 package com.stream;
 
+import com.stream.lambda.PrintSomething;
+
 public class StreamMain {
     public static void main(String[] args) {
         System.out.println("Exercises with functional programming - Java, interface Stream.");
+
+        PrintSomething printSomething = new PrintSomething();
+        printSomething.printSomething();
     }
 }
