7d866d8f661b72d877672d7b9248acc83cb574a1
MichalGuz <michalguz@gmail.com>
2019-06-27 23:19:47 +0200

    Create new package and class with method main.

---
+++ src/main/java/com/stream/StreamMain.java
@@ -0,0 +1,7 @@
+package com.stream;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("Exercises with functional programming - Java, interface Stream.");
+    }
+}
