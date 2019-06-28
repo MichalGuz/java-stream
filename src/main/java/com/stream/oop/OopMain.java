a341ff8aeb7d4dda826edb3550e70ac0e5a88cf9
MichalGuz <michalguz@gmail.com>
2019-06-29 00:30:27 +0200

    Refactor class 'StreamMain', rename for 'OopMain', because this package contains an example of object oriented programming.

--- src/main/java/com/stream/oop/OopMain.java
+++ src/main/java/com/stream/oop/OopMain.java
@@ -1,6 +1,6 @@
 package com.stream.oop;
 
-public class StreamMain {
+public class OopMain {
     public static void main(String[] args) {
         Processor processor = new Processor();
         ExecutePrintSomething executePrintSomething = new ExecutePrintSomething();
