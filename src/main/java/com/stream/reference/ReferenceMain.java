5c0b4430624c864ffa6bbfbef986691fb0defc6b
MichalGuz <michalguz@gmail.com>
2019-06-29 12:48:16 +0200

    Complete the implementation of main() method using method references.

--- src/main/java/com/stream/reference/ReferenceMain.java
+++ src/main/java/com/stream/reference/ReferenceMain.java
@@ -10,6 +10,10 @@ public class ReferenceMain {
         calculatorExecutor.executeCalculator(6, 3, (a, b) -> a * b);
         calculatorExecutor.executeCalculator(6, 3, (a, b) -> a / b);
 
-
+        System.out.println("\nCalculating expressions with method references:");
+        calculatorExecutor.executeCalculator(15, 3, FuncionalCalculator :: addAtoB);
+        calculatorExecutor.executeCalculator(15, 3, FuncionalCalculator :: subtractBfromA);
+        calculatorExecutor.executeCalculator(15, 3, FuncionalCalculator :: multiplyAByB);
+        calculatorExecutor.executeCalculator(15, 3, FuncionalCalculator :: divideAByB);
     }
 }
