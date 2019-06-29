8ee263625ee0db3720abe4492ebd0dcf547632fa
MichalGuz <michalguz@gmail.com>
2019-06-29 12:16:08 +0200

    Create first static method of funcional calculator. Add comment about static methods.

--- src/main/java/com/stream/reference/FuncionalCalculator.java
+++ src/main/java/com/stream/reference/FuncionalCalculator.java
@@ -1,4 +1,8 @@
 package com.stream.reference;
 
 public class FuncionalCalculator {
+    // create static methods - static methods needn't creating of new object to using them
+    public static double addAtoB(double a, double b){
+        return a + b;
+    }
 }
