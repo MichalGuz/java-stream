8386062cb08467f4b6b981a1ad88892caf50b976
MichalGuz <michalguz@gmail.com>
2019-06-29 12:43:03 +0200

    Implement main() method using lambda expressions.

--- src/main/java/com/stream/reference/ReferenceMain.java
+++ src/main/java/com/stream/reference/ReferenceMain.java
@@ -2,6 +2,14 @@ package com.stream.reference;
 
 public class ReferenceMain {
     public static void main(String[] args) {
+        CalculatorExecutor calculatorExecutor = new CalculatorExecutor();
+
+        System.out.println("Calculating expressions with lambdas:");
+        calculatorExecutor.executeCalculator(6, 3, (a, b) -> a + b);
+        calculatorExecutor.executeCalculator(6, 3, (a, b) -> a - b);
+        calculatorExecutor.executeCalculator(6, 3, (a, b) -> a * b);
+        calculatorExecutor.executeCalculator(6, 3, (a, b) -> a / b);
+
 
     }
 }
