eaa77b05dcdcdce7d9f7f901c10c4788d629f09c
MichalGuz <michalguz@gmail.com>
2019-06-29 12:00:29 +0200

    Creat new clas with main() method. Implement this method with executing method of class ExpressionExecutor with alambda expression and parameters.

---
+++ src/main/java/com/stream/calculator/CalculatorMain.java
@@ -0,0 +1,10 @@
+package com.stream.calculator;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+
+    }
+}
