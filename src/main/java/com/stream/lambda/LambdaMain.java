a91b9f3aba5e07e16fffec961e2d1c1a05d0dbbc
MichalGuz <michalguz@gmail.com>
2019-06-29 11:36:51 +0200

    Add comment with an important principle.

--- src/main/java/com/stream/lambda/LambdaMain.java
+++ src/main/java/com/stream/lambda/LambdaMain.java
@@ -3,6 +3,8 @@ package com.stream.lambda;
 public class LambdaMain {
     public static void main(String[] args) {
         LambdaProcessor lambdaProcessor = new LambdaProcessor();
+
+        //you may use lambda expression when interface has only the one method
         lambdaProcessor.lambdaProcess(() -> System.out.println("Exercise #2 - using lambda instead of OOP"));
     }
 }
