7dc4e7e7db2e9347033ec2c0ca98227fa53bab33
MichalGuz <michalguz@gmail.com>
2019-07-01 22:45:28 +0200

    Implement method main().

--- src/main/java/com/stream/iterate/StreamMain.java
+++ src/main/java/com/stream/iterate/StreamMain.java
@@ -2,6 +2,7 @@ package com.stream.iterate;
 
 public class StreamMain {
     public static void main(String[] args) {
-
+        System.out.println("Using Stream to generate even numbers rom 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
