b97ce842f34be46fd995814332fa54a0c893f82c
MichalGuz <michalguz@gmail.com>
2019-07-02 22:57:45 +0200

    Modify body of main() method - add map() method.

--- src/main/java/com/stream/person/PersonMain.java
+++ src/main/java/com/stream/person/PersonMain.java
@@ -3,6 +3,7 @@ package com.stream.person;
 public class PersonMain {
     public static void main(String[] args) {
         People.getList().stream()
+                .map(p -> p.toUpperCase())
                 .forEach(System.out::println);
     }
 }
