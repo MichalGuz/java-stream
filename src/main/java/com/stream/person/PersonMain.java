049a4d26b49ad12b093175919b3f527bc99abb5c
MichalGuz <michalguz@gmail.com>
2019-07-02 22:36:43 +0200

    Implement method main with methods getList(), stream() and terminal operation forEach() - print out full list of people.

--- src/main/java/com/stream/person/PersonMain.java
+++ src/main/java/com/stream/person/PersonMain.java
@@ -2,6 +2,7 @@ package com.stream.person;
 
 public class PersonMain {
     public static void main(String[] args) {
-
+        People.getList().stream()
+                .forEach(System.out::println);
     }
 }
