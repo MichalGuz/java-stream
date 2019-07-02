f50bb80c032597e353adff01707d9eff736675a8
MichalGuz <michalguz@gmail.com>
2019-07-02 22:29:02 +0200

    Create body of class People: method getList() with its implementation.

--- src/main/java/com/stream/person/People.java
+++ src/main/java/com/stream/person/People.java
@@ -1,4 +1,22 @@
 package com.stream.person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class People {
+    public static List<String> getList(){
+        final List<String> theList = new ArrayList<>();
+        theList.add("Adam Clarke");
+        theList.add("Boris Big");
+        theList.add("Hugh Smith");
+        theList.add("Amon Lee");
+        theList.add("Penelope Gray");
+        theList.add("Susan Robinson");
+        theList.add("Lucy Rose");
+        theList.add("Kate Rogers");
+        theList.add("Linda Morrison");
+        theList.add("Agatha Dawson");
+        theList.add("Carrie Parker");
+        return new ArrayList<String>(theList);
+    }
 }
