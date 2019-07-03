7c7845144f93b90e56fca5dee4c91e018b2205ae
MichalGuz <michalguz@gmail.com>
2019-07-03 23:58:56 +0200

    Add constructor of class BookDirectory and fill the collection with objects of class Book

--- src/main/java/com/stream/book/BookDirectory.java
+++ src/main/java/com/stream/book/BookDirectory.java
@@ -5,4 +5,14 @@ import java.util.List;
 
 public final class BookDirectory{
     private final List<Book> bookList = new ArrayList<>();
+
+    public BookDirectory(){
+        bookList.add(new Book("Author1", "Book1", 2003));
+        bookList.add(new Book("Author2", "Book2", 2013));
+        bookList.add(new Book("Author3", "Book3", 2014));
+        bookList.add(new Book("Author4", "Book4", 1999));
+        bookList.add(new Book("Author5", "Book5", 2001));
+        bookList.add(new Book("Author6", "Book6", 2004));
+        bookList.add(new Book("Author7", "Book7", 1995));
+    }
 }
