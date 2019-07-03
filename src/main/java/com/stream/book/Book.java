dbd00d60e0102d77255e8798a2e72029b42ee8f5
MichalGuz <michalguz@gmail.com>
2019-07-03 22:48:01 +0200

    Add constructor in class Book with 'final' arguments for creating immutable objects to use in stream().

--- src/main/java/com/stream/book/Book.java
+++ src/main/java/com/stream/book/Book.java
@@ -4,5 +4,11 @@ public final class Book {
     private final String author;
     private final String title;
     private final int yearORelease;
+
+    public Book(final String author, final String title, final int yearORelease) {
+        this.author = author;
+        this.title = title;
+        this.yearORelease = yearORelease;
+    }
 }
 
