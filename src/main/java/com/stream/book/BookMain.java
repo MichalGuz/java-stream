08c907650eec37c9518669e0aa545567c015e285
MichalGuz <michalguz@gmail.com>
2019-07-04 00:06:54 +0200

    Start stream() with list of objects generated using method getList().

--- src/main/java/com/stream/book/BookMain.java
+++ src/main/java/com/stream/book/BookMain.java
@@ -3,5 +3,9 @@ package com.stream.book;
 public class BookMain {
     public static void main(String[] args) {
         BookDirectory bookDirectory = new BookDirectory();
-        }
+        bookDirectory.getList().stream()
+                .filter(book -> book.getYearORelease() > 2010)
+                .forEach(System.out::println);
+
+    }
 }
