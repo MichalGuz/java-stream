d68ba8819a014faf5e7dbeb960147d02a37820f0
MichalGuz <michalguz@gmail.com>
2019-07-05 00:29:20 +0200

    Create new class ScooterSearch and initialiaze variable for collection of Scooters.

---
+++ src/main/java/com/stream/scooter/ScooterSearch.java
@@ -0,0 +1,10 @@
+package com.stream.scooter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ScooterSearch {
+    private final List<Scooter> list = new ArrayList<>();
+
+
+}
