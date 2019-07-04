d2b9b944330f8ce9952dd4c375b97087970aea07
MichalGuz <michalguz@gmail.com>
2019-07-04 22:35:26 +0200

    Try a diferent way of implementation the main() method with terminal operation collect().

--- src/main/java/com/stream/book/BookMain.java
+++ src/main/java/com/stream/book/BookMain.java
@@ -1,11 +1,19 @@
 package com.stream.book;
 
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BookMain {
     public static void main(String[] args) {
         BookDirectory bookDirectory = new BookDirectory();
         bookDirectory.getList().stream()
                 .filter(book -> book.getYearORelease() > 2010)
                 .forEach(System.out::println);
+
+        List<Book> theResultList = bookDirectory.getList().stream()
+                .filter(book -> book.getYearORelease() >2010)
+                .collect(Collectors.toList())
 
     }
 }
