242fb55973379fcb271690a43d4c8575069b2d7e
MichalGuz <michalguz@gmail.com>
2019-07-04 23:57:26 +0200

    Declare set of variables in class Scooter

--- src/main/java/com/stream/scooter/Scooter.java
+++ src/main/java/com/stream/scooter/Scooter.java
@@ -1,6 +1,14 @@
 package com.stream.scooter;
 
 public final class Scooter {
+    private final int id;
+    private final String brand;
+    private final String model;
+    private final int powerLevel;
+    private final boolean docked;
+    private final int locationDistance;
+    private final String status;
+
 }
 
 
