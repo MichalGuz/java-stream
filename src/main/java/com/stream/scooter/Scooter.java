113af84f3cb4078396e016a8aeb6275071b8c00a
MichalGuz <michalguz@gmail.com>
2019-07-04 23:58:59 +0200

    Create constructor, methods get() and method toString() in class Scooter.

--- src/main/java/com/stream/scooter/Scooter.java
+++ src/main/java/com/stream/scooter/Scooter.java
@@ -9,6 +9,56 @@ public final class Scooter {
     private final int locationDistance;
     private final String status;
 
+    public Scooter(int id, String brand, String model, int powerLevel, boolean docked, int locationDistance, String status) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.powerLevel = powerLevel;
+        this.docked = docked;
+        this.locationDistance = locationDistance;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPowerLevel() {
+        return powerLevel;
+    }
+
+    public boolean isDocked() {
+        return docked;
+    }
+
+    public int getLocationDistance() {
+        return locationDistance;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "Scooter{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", powerLevel=" + powerLevel +
+                ", docked=" + docked +
+                ", locationDistance=" + locationDistance +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
 
 
