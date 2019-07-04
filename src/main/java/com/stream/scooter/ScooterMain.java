9caefaf7e759c885166cd00a8890a0a773328be2
MichalGuz <michalguz@gmail.com>
2019-07-05 01:26:26 +0200

    Printing the results of searching available scooter (using stream()).

--- src/main/java/com/stream/scooter/ScooterMain.java
+++ src/main/java/com/stream/scooter/ScooterMain.java
@@ -7,12 +7,15 @@ public class ScooterMain {
     public static void main(String[] args) {
         ScooterSearch scooterSearch = new ScooterSearch();
 
-        Map<Integer, Scooter> theMap = scooterSearch.getList().stream()
+        Map<Integer, Scooter> theResultMap = scooterSearch.getList().stream()
                 .filter(scooter -> scooter.getPowerLevel() > 50)
                 .filter(scooter -> scooter.getLocationDistance() < 2)
                 .filter(scooter -> scooter.getStatus() == "free")
                 .collect(Collectors.toMap(Scooter::getId, scooter -> scooter));
 
-
+        System.out.println("Number of found scooters: " + theResultMap.size());
+        theResultMap.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry. getValue())
+                .forEach(System.out::println);
     }
 }
