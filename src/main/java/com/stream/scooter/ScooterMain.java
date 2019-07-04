f09852a58e6686e6cd9bd3af4b97e732b53cf393
MichalGuz <michalguz@gmail.com>
2019-07-05 01:17:52 +0200

    Implement the method main() using stream() with toMap().

--- src/main/java/com/stream/scooter/ScooterMain.java
+++ src/main/java/com/stream/scooter/ScooterMain.java
@@ -1,7 +1,18 @@
 package com.stream.scooter;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ScooterMain {
     public static void main(String[] args) {
+        ScooterSearch scooterSearch = new ScooterSearch();
+
+        Map<Integer, Scooter> theMap = scooterSearch.getList().stream()
+                .filter(scooter -> scooter.getPowerLevel() > 50)
+                .filter(scooter -> scooter.getLocationDistance() < 2)
+                .filter(scooter -> scooter.getStatus() == "free")
+                .collect(Collectors.toMap(Scooter::getId, scooter -> scooter));
+
 
     }
 }
