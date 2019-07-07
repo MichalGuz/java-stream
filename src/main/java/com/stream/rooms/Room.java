b6f53b75093c1a5e9135709831a997fa27482891
MichalGuz <michalguz@gmail.com>
2019-07-07 23:56:17 +0200

    Generate constructor in class Room.'

--- src/main/java/com/stream/rooms/Room.java
+++ src/main/java/com/stream/rooms/Room.java
@@ -5,4 +5,11 @@ public final class Room {
     private final String roomSize;
     private final boolean roomAvailable;
     private final String reservation;
+
+    public Room(int roomNumber, String roomSize, boolean roomAvailable, String reservation) {
+        this.roomNumber = roomNumber;
+        this.roomSize = roomSize;
+        this.roomAvailable = roomAvailable;
+        this.reservation = reservation;
+    }
 }
