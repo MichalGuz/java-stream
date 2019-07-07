e322bfcdfdc5f962f4cbedbb95c1e72dc852e94b
MichalGuz <michalguz@gmail.com>
2019-07-07 23:55:37 +0200

    Create fields in class Room - without initailization.'

--- src/main/java/com/stream/rooms/Room.java
+++ src/main/java/com/stream/rooms/Room.java
@@ -1,4 +1,8 @@
 package com.stream.rooms;
 
 public final class Room {
+    private final int roomNumber;
+    private final String roomSize;
+    private final boolean roomAvailable;
+    private final String reservation;
 }
