0a6a1d825d0dc2cefbebcbaf9166fdc0fe2a0a30
MichalGuz <michalguz@gmail.com>
2019-07-08 00:41:12 +0200

    Create new object of class RoomCheck.

--- src/main/java/com/stream/rooms/RoomMain.java
+++ src/main/java/com/stream/rooms/RoomMain.java
@@ -1,7 +1,11 @@
 package com.stream.rooms;
 
+import java.util.Map;
+
 public class RoomMain {
     public static void main(String[] args) {
+        RoomCheck roomCheck = new RoomCheck();
 
+        Map<Integer, Room> roomMap = roomCheck.getList().stream()
     }
 }
