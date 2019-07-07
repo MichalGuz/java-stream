792703b45ceb64a313f13f0f9b5e2e91f75faa6d
MichalGuz <michalguz@gmail.com>
2019-07-07 23:58:05 +0200

    Generate get() methods in class Room.'

--- src/main/java/com/stream/rooms/Room.java
+++ src/main/java/com/stream/rooms/Room.java
@@ -12,4 +12,20 @@ public final class Room {
         this.roomAvailable = roomAvailable;
         this.reservation = reservation;
     }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getRoomSize() {
+        return roomSize;
+    }
+
+    public boolean isRoomAvailable() {
+        return roomAvailable;
+    }
+
+    public String getReservation() {
+        return reservation;
+    }
 }
