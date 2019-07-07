e71f4285633d343d209f379be204bb8401c629bc
MichalGuz <michalguz@gmail.com>
2019-07-08 00:26:56 +0200

    Create constructor in class RoomCheck and fill the collecion of Room objects in that constructor.

--- src/main/java/com/stream/rooms/RoomCheck.java
+++ src/main/java/com/stream/rooms/RoomCheck.java
@@ -5,4 +5,13 @@ import java.util.List;
 
 public final class RoomCheck {
     private final List<Room> roomList = new ArrayList<>();
+
+    public RoomCheck(){
+        roomList.add(new Room(1, "double", false, "Mr John Smith"));
+        roomList.add(new Room(2, "double", false, "Ms Mia Jackson"));
+        roomList.add(new Room(3, "single", false, "Mr Toby Jones"));
+        roomList.add(new Room(4, "double", false, "Ms Helen Osborn"));
+        roomList.add(new Room(5, "double", false, "Ms Lisa Parker"));
+        roomList.add(new Room(6, "single", true, ""));
+    }
 }
