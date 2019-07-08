a9bd58656ab306489371e8a894ba1ba38f8dfb71
MichalGuz <michalguz@gmail.com>
2019-07-08 23:52:17 +0200

    Declare variables in class ForumUser.

--- src/main/java/com/stream/forumuser/ForumUser.java
+++ src/main/java/com/stream/forumuser/ForumUser.java
@@ -1,4 +1,11 @@
 package com.stream.forumuser;
 
+import java.time.LocalDate;
+
 public final class ForumUser {
+    private final int id;
+    private final String name;
+    private final char sex;
+    private final LocalDate dateOBirth;
+    private final int postsQuantity;
 }
