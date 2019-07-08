6f8504b3ec35aa104eef626e3eb1f05693ae6048
MichalGuz <michalguz@gmail.com>
2019-07-08 23:57:28 +0200

    Create new class refers to collecion of objects of class ForumUser.

---
+++ src/main/java/com/stream/forumuser/Forum.java
@@ -0,0 +1,9 @@
+package com.stream.forumuser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Forum {
+    private final List<ForumUser> forumUserList = new ArrayList<>();
+
+}
