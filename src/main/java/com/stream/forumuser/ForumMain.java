3504c65ff64bd59b657d91ac85ecf3029bb8f421
MichalGuz <michalguz@gmail.com>
2019-07-09 23:57:10 +0200

    Declare new variable of Map() class..

--- src/main/java/com/stream/forumuser/ForumMain.java
+++ src/main/java/com/stream/forumuser/ForumMain.java
@@ -1,7 +1,10 @@
 package com.stream.forumuser;
 
+import java.util.Map;
+
 public class ForumMain {
     public static void main(String[] args) {
         Forum forum = new Forum();
+        Map<Integer, ForumUser> resultsMap
     }
 }
