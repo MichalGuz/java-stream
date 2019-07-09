2d55b0796bf1a7c3de10a8bceb387fc07bf716a4
MichalGuz <michalguz@gmail.com>
2019-07-09 23:52:34 +0200

    Continue practice of stream(), exercice 'Forum'. Delete redundant brackets.

--- src/main/java/com/stream/forumuser/Forum.java
+++ src/main/java/com/stream/forumuser/Forum.java
@@ -13,9 +13,9 @@ public final class Forum {
         forumUserList.add(new ForumUser(15, "User3", 'm', LocalDate.of(1967,3,10),98));
         forumUserList.add(new ForumUser(23, "User4", 'f', LocalDate.of(1981,10,27),14));
         forumUserList.add(new ForumUser(26, "User5", 'm', LocalDate.of(1989,6,20),43));
-        forumUserList.add(new ForumUser(30, "User6", 'f', LocalDate.of(1999,8,15),43));
-        forumUserList.add(new ForumUser(32, "User7", 'f', LocalDate.of(1992,9,4),53);
-        forumUserList.add(new ForumUser(41, "User8", 'm', LocalDate.of(1983,1,9), 15);
+        forumUserList.add(new ForumUser(30, "User6", 'f', LocalDate.of(1999,8,15),39));
+        forumUserList.add(new ForumUser(32, "User7", 'f', LocalDate.of(1992,9,4),53));
+        forumUserList.add(new ForumUser(41, "User8", 'm', LocalDate.of(1983,1,9), 15));
         forumUserList.add(new ForumUser(46, "User9", 'f', LocalDate.of(1977,11,21),17));
         forumUserList.add(new ForumUser(47, "User0", 'm', LocalDate.of(1968,7,19),26));
     }
