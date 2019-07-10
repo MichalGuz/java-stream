c68c6905ac61b094336248af7b0175fdedc3004f
MichalGuz <michalguz@gmail.com>
2019-07-10 22:14:49 +0200

    Add a terminal method: collect () to create a Map with new objects from the stream that have met the filter condition.

--- src/main/java/com/stream/forumuser/ForumMain.java
+++ src/main/java/com/stream/forumuser/ForumMain.java
@@ -3,6 +3,7 @@ package com.stream.forumuser;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ForumMain {
     public static void main(String[] args) {
@@ -12,6 +13,7 @@ public class ForumMain {
                 .filter(user -> user.getSex() == 'M')
                 .filter(user -> user.getDateOBirth().getYear() - LocalDate.now().getYear() > 19)
                 .filter(user -> user.getPostsQuantity() > 0)
+                .collect(Collectors.toMap(ForumUser::getId, user -> user));
 
     }
 }
