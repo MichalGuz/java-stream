97a59414f0322c7648e065f0c3f0c41cdbd6f256
MichalGuz <michalguz@gmail.com>
2019-07-10 22:45:12 +0200

    Change arguments of constructor in class Forum.

--- src/main/java/com/stream/forumuser/Forum.java
+++ src/main/java/com/stream/forumuser/Forum.java
@@ -8,16 +8,16 @@ public final class Forum {
     private final List<ForumUser> forumUserList = new ArrayList<>();
 
     public Forum(){
-        forumUserList.add(new ForumUser(10, "User1", 'f', LocalDate.of(1988,6,20),10));
-        forumUserList.add(new ForumUser(13, "User2", 'm', LocalDate.of(1998,5,24),45));
-        forumUserList.add(new ForumUser(15, "User3", 'm', LocalDate.of(1967,3,10),98));
-        forumUserList.add(new ForumUser(23, "User4", 'f', LocalDate.of(1981,10,27),14));
-        forumUserList.add(new ForumUser(26, "User5", 'm', LocalDate.of(1989,6,20),43));
-        forumUserList.add(new ForumUser(30, "User6", 'f', LocalDate.of(1999,8,15),39));
-        forumUserList.add(new ForumUser(32, "User7", 'f', LocalDate.of(1992,9,4),53));
-        forumUserList.add(new ForumUser(41, "User8", 'm', LocalDate.of(1983,1,9), 15));
-        forumUserList.add(new ForumUser(46, "User9", 'f', LocalDate.of(1977,11,21),17));
-        forumUserList.add(new ForumUser(47, "User0", 'm', LocalDate.of(1968,7,19),26));
+        forumUserList.add(new ForumUser(10, "User1", 'f', 1988,6,20,10));
+        forumUserList.add(new ForumUser(13, "User2", 'm', 1998,5,24,45));
+        forumUserList.add(new ForumUser(15, "User3", 'm', 1967,3,10,98));
+        forumUserList.add(new ForumUser(23, "User4", 'f', 1981,10,27,14));
+        forumUserList.add(new ForumUser(26, "User5", 'm', 1989,6,20,43));
+        forumUserList.add(new ForumUser(30, "User6", 'f', 1999,8,15,39));
+        forumUserList.add(new ForumUser(32, "User7", 'f', 1992,9,4,53));
+        forumUserList.add(new ForumUser(41, "User8", 'm', 1983,1,9, 15));
+        forumUserList.add(new ForumUser(46, "User9", 'f', 1977,11,21,17));
+        forumUserList.add(new ForumUser(47, "User0", 'm', 1968,7,19,26));
     }
 
     public List<ForumUser> getForumUserList(){
