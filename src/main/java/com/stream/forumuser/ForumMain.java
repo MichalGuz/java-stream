325090605378510b9f526ec1db169fc8e2634697
MichalGuz <michalguz@gmail.com>
2019-07-10 22:05:57 +0200

    Add filter() method in the stream to select users who have posted one post or more.

--- src/main/java/com/stream/forumuser/ForumMain.java
+++ src/main/java/com/stream/forumuser/ForumMain.java
@@ -11,6 +11,7 @@ public class ForumMain {
         Map<Integer, ForumUser> resultsMap = forum.getForumUserList().stream()
                 .filter(user -> user.getSex() == 'M')
                 .filter(user -> user.getDateOBirth().getYear() - LocalDate.now().getYear() > 19)
+                .filter(user -> user.getPostsQuantity() > 0)
 
     }
 }
