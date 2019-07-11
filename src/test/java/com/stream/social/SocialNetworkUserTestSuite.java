3c8772eb4e78a904ae00fdaab16801be953b211a
MichalGuz <michalguz@gmail.com>
2019-07-11 23:59:32 +0200

    Create new objects of class SocialNetworkUser and fill the collection with them.

--- src/test/java/com/stream/social/SocialNetworkUserTestSuite.java
+++ src/test/java/com/stream/social/SocialNetworkUserTestSuite.java
@@ -5,6 +5,18 @@ import org.junit.Test;
 public class SocialNetworkUserTestSuite {
     @Test
     public void testGetLocationsOfFriends(){
+        // given
+        // create users
+        SocialNetworkUser user1 = new SocialNetworkUser("user1", "location1", 20);
+        SocialNetworkUser user2 = new SocialNetworkUser("user2", "location2", 30);
+        SocialNetworkUser user3 = new SocialNetworkUser("user3", "location3", 25);
+        SocialNetworkUser user4 = new SocialNetworkUser("user4", "location4", 20);
+        SocialNetworkUser user5 = new SocialNetworkUser("user5", "location5", 27);
+        SocialNetworkUser user6 = new SocialNetworkUser("user6", "location6", 30);
+        SocialNetworkUser user7 = new SocialNetworkUser("user7", "location7", 40);
+        SocialNetworkUser user8 = new SocialNetworkUser("user8", "location8", 35);
+        SocialNetworkUser user9 = new SocialNetworkUser("user9", "location9", 25);
+        SocialNetworkUser user10 = new SocialNetworkUser("user10", "location10", 44);
 
     }
 }
