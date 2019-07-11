0449eb699dad9b77ffa238100c21fad539f6f7f5
MichalGuz <michalguz@gmail.com>
2019-07-11 22:01:25 +0200

    Declare variables in class SocialNetworkUser.

--- src/main/java/com/stream/social/SocialNetworkUser.java
+++ src/main/java/com/stream/social/SocialNetworkUser.java
@@ -1,5 +1,11 @@
 package com.stream.social;
 
-public final class SocialNetworkUser {
+import java.util.HashSet;
+import java.util.Set;
 
+public final class SocialNetworkUser {
+    private final String userName;
+    private final String location;
+    private final int age;
+    private final Set<SocialNetworkUser> friends = new HashSet<>();
 }
