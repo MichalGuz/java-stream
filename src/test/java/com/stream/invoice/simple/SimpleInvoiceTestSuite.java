cff4ed2f602b74183ab3e96fc9419574bcced347
MichalGuz <michalguz@gmail.com>
2019-07-13 22:38:44 +0200

    Create section 'then' of method GetValueToPay().

--- src/test/java/com/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ src/test/java/com/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -1,5 +1,6 @@
 package com.stream.invoice.simple;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SimpleInvoiceTestSuite {
@@ -10,7 +11,10 @@ public class SimpleInvoiceTestSuite {
 
         // when
         simpleInvoice.addItem(new SimpleItem(new SimpleProduct("Product1", 15.02), 2));
-        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product2", 14.84), 3.5);
+        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product2", 14.84), 3.5));
         simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product3", 8.42), 4.2));
+
+        // then
+        Assert.assertEquals(117.344, simpleInvoice.getValueToPay(), 0);
     }
 }
