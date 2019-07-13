c7265f389323c219a7d9c2b20ec137578fa3b971
MichalGuz <michalguz@gmail.com>
2019-07-13 21:58:02 +0200

    Create class SimpleItem with fields, constructor and get() methods.

---
+++ src/main/java/com/stream/invoice/simple/SimpleItem.java
@@ -0,0 +1,19 @@
+package com.stream.invoice.simple;
+
+public final class SimpleItem {
+    private final SimpleProduct product;
+    private final double quantity;
+
+    public SimpleItem(SimpleProduct product, double quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public SimpleProduct getProduct() {
+        return product;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+}
