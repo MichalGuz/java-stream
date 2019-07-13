f46fe9faf5601885861cd8a108592959eab1a6a2
MichalGuz <michalguz@gmail.com>
2019-07-13 22:02:04 +0200

    Create class SimpleInvoice and initialize new variables 'List<SimpleItem> items'.

---
+++ src/main/java/com/stream/invoice/simple/SimpleInvoice.java
@@ -0,0 +1,9 @@
+package com.stream.invoice.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SimpleInvoice {
+    private final List<SimpleItem> items = new ArrayList<>();
+
+}
