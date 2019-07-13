2a0f423c8845973962ddb2572439573cabef8390
MichalGuz <michalguz@gmail.com>
2019-07-13 19:22:54 +0200

    Create section 'when' in test with  new sets of friends for each user.

--- src/test/java/com/stream/social/SocialNetworkUserTestSuite.java
+++ src/test/java/com/stream/social/SocialNetworkUserTestSuite.java
@@ -88,6 +88,49 @@ public class SocialNetworkUserTestSuite {
         SocialNetworkUser user9 = new SocialNetworkUser("user9", "location1", 25);
         SocialNetworkUser user10 = new SocialNetworkUser("user10", "location2", 44);
 
+        // when
+        // create relations for user1
+        user1.addFriend(user2);
+        user1.addFriend(user6);
+        user1.addFriend(user9);
+        // create relations for user 2
+        user2.addFriend(user1);
+        user2.addFriend(user4);
+        user2.addFriend(user10);
+        // create relations for user 3
+        user3.addFriend(user5);
+        user3.addFriend(user7);
+        user3.addFriend(user9);
+        // create relations for user 4
+        user4.addFriend(user2);
+        user4.addFriend(user10);
+        // create relations for user 5
+        user5.addFriend(user3);
+        user5.addFriend(user6);
+        user5.addFriend(user8);
+        // create relations for user 6
+        user6.addFriend(user1);
+        user6.addFriend(user5);
+        user6.addFriend(user9);
+        // create relations for user 7
+        user7.addFriend(user3);
+        user7.addFriend(user8);
+        user7.addFriend(user9);
+        // create relations for user 8
+        user8.addFriend(user5);
+        user8.addFriend(user7);
+        user8.addFriend(user10);
+        // create relations for user 9
+        user9.addFriend(user1);
+        user9.addFriend(user3);
+        user9.addFriend(user6);
+        user9.addFriend(user7);
+        // create relations for user 10
+        user9.addFriend(user2);
+        user9.addFriend(user4);
+        user9.addFriend(user8);
+
+
 
     }
 }
