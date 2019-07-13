959d17ac3a1395a7c463f47307cd0c6105cbc96a
MichalGuz <michalguz@gmail.com>
2019-07-13 12:56:57 +0200

    Change argument 'location' in few objects.

--- src/test/java/com/stream/social/SocialNetworkUserTestSuite.java
+++ src/test/java/com/stream/social/SocialNetworkUserTestSuite.java
@@ -11,12 +11,14 @@ public class SocialNetworkUserTestSuite {
         SocialNetworkUser user2 = new SocialNetworkUser("user2", "location2", 30);
         SocialNetworkUser user3 = new SocialNetworkUser("user3", "location3", 25);
         SocialNetworkUser user4 = new SocialNetworkUser("user4", "location4", 20);
-        SocialNetworkUser user5 = new SocialNetworkUser("user5", "location5", 27);
-        SocialNetworkUser user6 = new SocialNetworkUser("user6", "location6", 30);
-        SocialNetworkUser user7 = new SocialNetworkUser("user7", "location7", 40);
-        SocialNetworkUser user8 = new SocialNetworkUser("user8", "location8", 35);
-        SocialNetworkUser user9 = new SocialNetworkUser("user9", "location9", 25);
-        SocialNetworkUser user10 = new SocialNetworkUser("user10", "location10", 44);
+        SocialNetworkUser user5 = new SocialNetworkUser("user5", "location1", 27);
+        SocialNetworkUser user6 = new SocialNetworkUser("user6", "location2", 30);
+        SocialNetworkUser user7 = new SocialNetworkUser("user7", "location3", 40);
+        SocialNetworkUser user8 = new SocialNetworkUser("user8", "location4", 35);
+        SocialNetworkUser user9 = new SocialNetworkUser("user9", "location1", 25);
+        SocialNetworkUser user10 = new SocialNetworkUser("user10", "location2", 44);
 
+        // when
+        // create relations for user1
     }
 }
