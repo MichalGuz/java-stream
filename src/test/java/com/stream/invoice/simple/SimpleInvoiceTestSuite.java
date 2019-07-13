25cc0fe0a66289ad93fb30f8019cfc65ad44103f
MichalGuz <michalguz@gmail.com>
2019-07-13 22:26:30 +0200

    Create section 'when' of method GetValueToPay().

--- src/test/java/com/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ src/test/java/com/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -5,6 +5,12 @@ import org.junit.Test;
 public class SimpleInvoiceTestSuite {
     @Test
     public void testGetValueToPay(){
+        // given
+        SimpleInvoice simpleInvoice = new SimpleInvoice();
 
+        // when
+        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("Product1", 15.02), 2));
+        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product2", 14.84), 3.5);
+        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product3", 8.42), 4.2));
     }
 }
