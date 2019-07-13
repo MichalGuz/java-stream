412aff389fdaba101c6773c923d110c2acdb378f
MichalGuz <michalguz@gmail.com>
2019-07-13 22:45:38 +0200

    Change delta in setion 'then' in test of method GetValueToPay().

--- src/test/java/com/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ src/test/java/com/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -10,11 +10,11 @@ public class SimpleInvoiceTestSuite {
         SimpleInvoice simpleInvoice = new SimpleInvoice();
 
         // when
-        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("Product1", 15.02), 2));
+        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("Product1", 15.99), 2));
         simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product2", 14.84), 3.5));
-        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product3", 8.42), 4.2));
+        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product3", 8.49), 4.2));
 
         // then
-        Assert.assertEquals(117.344, simpleInvoice.getValueToPay(), 0);
+        Assert.assertEquals(119.578, simpleInvoice.getValueToPay(), 0.001);
     }
 }
