5cb194a33b9274bf5ef49c50266b1eb5396acc7e
MichalGuz <michalguz@gmail.com>
2019-07-14 13:45:34 +0200

    Amend a value of productPrice in first object created for test. Change delta in assertEquals() method. Show an error of counting the variables of type 'double'.

--- src/test/java/com/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ src/test/java/com/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -10,11 +10,11 @@ public class SimpleInvoiceTestSuite {
         SimpleInvoice simpleInvoice = new SimpleInvoice();
 
         // when
-        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("Product1", 15.99), 2));
+        simpleInvoice.addItem(new SimpleItem(new SimpleProduct("Product1", 15.98), 2));
         simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product2", 14.84), 3.5));
         simpleInvoice.addItem(new SimpleItem(new SimpleProduct("product3", 8.49), 4.2));
 
         // then
-        Assert.assertEquals(119.578, simpleInvoice.getValueToPay(), 0.001);
+        Assert.assertEquals(119.578, simpleInvoice.getValueToPay(), 0);
     }
 }
