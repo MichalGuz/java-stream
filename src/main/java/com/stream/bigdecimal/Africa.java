598739632a9a5af61583ced719414a3aeff09447
MichalGuz <michalguz@gmail.com>
2019-07-14 14:10:39 +0200

    Modify the method of class Africa by introducing BigDecimal type of variable.

--- src/main/java/com/stream/bigdecimal/Africa.java
+++ src/main/java/com/stream/bigdecimal/Africa.java
@@ -1,8 +1,11 @@
 package com.stream.bigdecimal;
 
+import java.math.BigDecimal;
+
 public final class Africa implements SandStorage {
     @Override
-    public long getSandBeansQuantity() {
-        return 98765432109876543210L;
+    public BigDecimal getSandBeansQuantity() {
+        BigDecimal sandQuantity = new BigDecimal("98765432109876543210");
+        return sandQuantity;
     }
 }
