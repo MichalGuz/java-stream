373c3e10a5c741aeeac64c0cd8cb8143efbb740f
MichalGuz <michalguz@gmail.com>
2019-07-14 16:25:51 +0200

    Create section 'given' in test of method getSandBeansQuantity().

--- src/test/java/com/stream/bigdecimal/SandStorageTestSuite.java
+++ src/test/java/com/stream/bigdecimal/SandStorageTestSuite.java
@@ -1,5 +1,18 @@
 package com.stream.bigdecimal;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SandStorageTestSuite {
+    @Test
+    public void testGetSandBeansQuantity(){
+        // given
+        List<SandStorage> continents= new ArrayList<>();
+        continents.add(new Africa());
+        continents.add(new Asia());
+        continents.add(new Australia());
+    }
 
 }
