e88a9a7a52f8d952dc45f34ad65b0970c54034d1
MichalGuz <michalguz@gmail.com>
2019-07-14 14:19:30 +0200

    Create class Asia implements interface SandStorage.

---
+++ src/main/java/com/stream/bigdecimal/Asia.java
@@ -0,0 +1,12 @@
+package com.stream.bigdecimal;
+
+import java.math.BigDecimal;
+
+public final class Asia implements SandStorage{
+
+    @Override
+    public BigDecimal getSandBeansQuantity() {
+        BigDecimal sandQuantity = new BigDecimal("9999999999999993456732");
+        return sandQuantity;
+    }
+}
