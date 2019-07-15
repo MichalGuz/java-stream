d38ba5218b772c8f751f9fab2e797bfe3349c737
MichalGuz <michalguz@gmail.com>
2019-07-15 23:16:48 +0200

    Create method addCountry() in class Continent.

--- src/main/java/com/stream/world/Continent.java
+++ src/main/java/com/stream/world/Continent.java
@@ -5,4 +5,8 @@ import java.util.List;
 
 public class Continent {
     public List<Country> countries = new ArrayList<>();
+
+    public void addCountry(Country country){
+        countries.add(country);
+    }
 }
