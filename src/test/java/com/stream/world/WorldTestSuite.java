2f107e9d53d95bfd285c6ac8ccf69bb5d04c0f33
MichalGuz <michalguz@gmail.com>
2019-07-15 23:22:02 +0200

    Create method of test with section given.

--- src/test/java/com/stream/world/WorldTestSuite.java
+++ src/test/java/com/stream/world/WorldTestSuite.java
@@ -1,4 +1,18 @@
 package com.stream.world;
 
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WorldTestSuite {
+    @Test
+    public void testGetPeopleQuantity(){
+        // given
+        List<Country> europe = new ArrayList<>();
+        europe.add(new Country("Poland", new BigDecimal("38000000")));
+        europe.add(new Country("Germany", new BigDecimal("81000000")));
+        europe.add(new Country("France", new BigDecimal("65000000")));
+        europe.add(new Country("Italy", new BigDecimal("60000000")));
 }
