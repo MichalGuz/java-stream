1b5c60fef94a6d438bf12e2769af7b2cf153dc8f
MichalGuz <michalguz@gmail.com>
2019-07-15 23:05:26 +0200

    Override method getPeopleQuantity in class World.

--- src/main/java/com/stream/world/World.java
+++ src/main/java/com/stream/world/World.java
@@ -1,8 +1,15 @@
 package com.stream.world;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class World {
+public class World extends Country{
     List<Continent> continents = new ArrayList<>();
+
+    @Override
+    public BigDecimal getPeopleQuantity(BigDecimal peopleQuantity){
+        return peopleQuantity;
+    }
+
 }
