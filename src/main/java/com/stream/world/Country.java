b401799bcc85591f11b1840358874aa5898007e0
MichalGuz <michalguz@gmail.com>
2019-07-15 23:10:52 +0200

    Add a field in class Country.

--- src/main/java/com/stream/world/Country.java
+++ src/main/java/com/stream/world/Country.java
@@ -3,6 +3,7 @@ package com.stream.world;
 import java.math.BigDecimal;
 
 public class Country {
+    BigDecimal peopleQuantity;
 
     public BigDecimal getPeopleQuantity(){
         return peopleQuantity;
