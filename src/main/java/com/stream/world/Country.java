54b0cba107bcd2394ab4f7655d906a95702450ee
MichalGuz <michalguz@gmail.com>
2019-07-15 23:18:06 +0200

    Create new field and constructor in class Country.

--- src/main/java/com/stream/world/Country.java
+++ src/main/java/com/stream/world/Country.java
@@ -3,7 +3,13 @@ package com.stream.world;
 import java.math.BigDecimal;
 
 public class Country {
+    String name;
     BigDecimal peopleQuantity;
+
+    public Country(String name, BigDecimal peopleQuantity) {
+        this.name = name;
+        this.peopleQuantity = peopleQuantity;
+    }
 
     public BigDecimal getPeopleQuantity(){
         return peopleQuantity;
