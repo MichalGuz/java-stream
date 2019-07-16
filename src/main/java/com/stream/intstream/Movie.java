97ecee625d951cbd96430f8ed44dbf31c1c9eb01
MichalGuz <michalguz@gmail.com>
2019-07-16 22:41:06 +0200

    Create costructor of Movie class.

--- src/main/java/com/stream/intstream/Movie.java
+++ src/main/java/com/stream/intstream/Movie.java
@@ -4,4 +4,12 @@ public final class Movie {
     private final String title;
     private final int yearORelease;
     private final int lenghtInMinutes;
+
+    public Movie(final  String title, final int yearORelease, final int lenghtInMinutes) {
+        this.title = title;
+        this.yearORelease = yearORelease;
+        this.lenghtInMinutes = lenghtInMinutes;
+    }
+
+
 }
