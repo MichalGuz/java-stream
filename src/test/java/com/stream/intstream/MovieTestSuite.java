fb5e69d8383221513e9395e4000f425aa51bdff2
MichalGuz <michalguz@gmail.com>
2019-07-16 23:15:09 +0200

    Create 'when' section in testGetListUsing().

--- src/test/java/com/stream/intstream/MovieTestSuite.java
+++ src/test/java/com/stream/intstream/MovieTestSuite.java
@@ -2,10 +2,16 @@ package com.stream.intstream;
 
 import org.junit.Test;
 
+import java.util.List;
+
 public class MovieTestSuite {
     @Test
-    public void testGetListUsing(){
+    public void testGetListUsing() {
         // given
+        MovieCollection movieCollection = new MovieCollection();
+
+        // when
+        List<Movie> movies = movieCollection.getMovies();
 
     }
 }
