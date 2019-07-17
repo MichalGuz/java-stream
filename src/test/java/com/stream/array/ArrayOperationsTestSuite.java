65d329efa9654290927e24d043346fc1ed70e81a
MichalGuz <michalguz@gmail.com>
2019-07-17 22:21:42 +0200

    Add section 'given' in the method testGetAverage(): create a table and fill it using a loop.

--- src/test/java/com/stream/array/ArrayOperationsTestSuite.java
+++ src/test/java/com/stream/array/ArrayOperationsTestSuite.java
@@ -4,7 +4,13 @@ import org.junit.Test;
 
 public class ArrayOperationsTestSuite {
     @Test
-    public void testGetAverage(){
+    public void testGetAverage() {
+        // given
+        int[] numbers = new int[6];
+        for (int i = 0; i < 7; i++) {
+            numbers[i] = i;
+            i++;
+        }
 
     }
 }
