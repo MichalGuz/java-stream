fff1191086549c3c7ca1d72ba7610e4f05d8e0b6
MichalGuz <michalguz@gmail.com>
2019-07-17 21:03:10 +0200

    Add body of the static method getAverage(): first IntStream for printing numbers.

--- src/main/java/com/stream/array/ArrayOperations.java
+++ src/main/java/com/stream/array/ArrayOperations.java
@@ -1,7 +1,10 @@
 package com.stream.array;
 
+import java.util.stream.IntStream;
+
 public interface ArrayOperations {
     static double getAverage(int[] numbers){
-
+        IntStream.range(0, numbers.length)
+                .forEach(n -> System.out.println(n));
     }
 }
