1a79b253bee1cce4039214f5c409b0a3cd79cabb
MichalGuz <michalguz@gmail.com>
2019-07-18 23:56:25 +0200

    Add methods: addTaskList() and removeTaskList() in class Board.

--- src/main/java/com/stream/task/Board.java
+++ src/main/java/com/stream/task/Board.java
@@ -10,4 +10,12 @@ public final class Board {
     public Board(String name) {
         this.name = name;
     }
+
+    public void addTaskList(TaskList taskList){
+        taskLists.add(taskList);
+    }
+
+    private boolean removeTaskList(TaskList taskList){
+        return taskLists.remove(taskList);
+    }
 }
