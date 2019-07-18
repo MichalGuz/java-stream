d507a9434322cdd3f3957dda037bf18c9414c8e0
MichalGuz <michalguz@gmail.com>
2019-07-18 21:31:01 +0200

    Create TaskList class with fields.

---
+++ src/main/java/com/stream/task/TaskList.java
@@ -0,0 +1,9 @@
+package com.stream.task;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public final class TaskList {
+    private final List<Task> tasks = new LinkedList<>();
+    private final String name;
+}
