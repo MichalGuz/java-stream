34e608a9d2818ba098aaf7e923772cdadfb80eda
MichalGuz <michalguz@gmail.com>
2019-07-18 21:53:27 +0200

    Add keyword 'final' in constructor of class TaskList.

--- src/main/java/com/stream/task/TaskList.java
+++ src/main/java/com/stream/task/TaskList.java
@@ -7,7 +7,7 @@ public final class TaskList {
     private final List<Task> tasks = new LinkedList<>();
     private final String name;
 
-    public TaskList(String name) {
+    public TaskList(final String name) {
         this.name = name;
     }
 }
