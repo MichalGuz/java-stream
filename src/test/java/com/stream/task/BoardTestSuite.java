58848f080952c63a3053740d330d6346ef6e32ef
MichalGuz <michalguz@gmail.com>
2019-07-21 23:58:48 +0200

    Create method prepareTestData() to prepare data for test. Create new User objects.

--- src/test/java/com/stream/task/BoardTestSuite.java
+++ src/test/java/com/stream/task/BoardTestSuite.java
@@ -1,4 +1,11 @@
 package com.stream.task;
 
 public class BoardTestSuite {
+    public Board prepeareTestData(){
+        // users
+        User user1 = new User("user1", "Adam Smith");
+        User user2 = new User("user2", "John Glenn");
+        User user3 = new User("user3", "Peter Johnson");
+        User user4 = new User("user4", "Anna Adams");
+    }
 }
