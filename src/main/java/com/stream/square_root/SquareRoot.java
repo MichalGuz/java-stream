22b1ebcd0eb4b6810ded900671c1388f53564caf
MichalGuz <michalguz@gmail.com>
2019-10-12 23:30:57 +0200

    Start stream of numbers in  method practice() in class SquareRoot.

--- src/main/java/com/stream/square_root/SquareRoot.java
+++ src/main/java/com/stream/square_root/SquareRoot.java
@@ -1,5 +1,9 @@
 package com.stream.square_root;
 
+import java.util.stream.IntStream;
+
 public class SquareRoot {
-    public Long practice(){}
+    public Long practice(){
+        return IntStream.range(1, 25)
+    }
 }
