410849ec965ac08682568695d11d1f982b850798
MichalGuz <michalguz@gmail.com>
2019-10-12 23:31:53 +0200

    Map the IntStream object to Object in  method practice().

--- src/main/java/com/stream/square_root/SquareRoot.java
+++ src/main/java/com/stream/square_root/SquareRoot.java
@@ -5,5 +5,6 @@ import java.util.stream.IntStream;
 public class SquareRoot {
     public Long practice(){
         return IntStream.range(1, 25)
+                .mapToObj(x ->IntStream.of(IntStream.range(0,x).sum(), IntStream.range(0,x+1).sum()))
     }
 }
