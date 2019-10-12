9d4dbd4d0d3f7ff0482a6951ede84b59b0a571ea
MichalGuz <michalguz@gmail.com>
2019-10-12 23:35:02 +0200

    Filter the Integers (when modulo of their square root equals zero)  in  method practice().

--- src/main/java/com/stream/square_root/SquareRoot.java
+++ src/main/java/com/stream/square_root/SquareRoot.java
@@ -7,5 +7,6 @@ public class SquareRoot {
         return IntStream.range(1, 25)
                 .mapToObj(x ->IntStream.of(IntStream.range(0,x).sum(), IntStream.range(0,x+1).sum()))
                 .map(a -> a.sum())
+                .filter(x -> Math.sqrt(x)%1 == 0)
     }
 }
