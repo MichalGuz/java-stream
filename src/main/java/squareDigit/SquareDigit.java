b70416c9de4ac9958c03582d99e18b2bf920b8b2
MichalGuz <michalguz@gmail.com>
2020-02-11 23:38:21 +0100

    Create an List<String> in the SquareDigit class.

--- src/main/java/squareDigit/SquareDigit.java
+++ src/main/java/squareDigit/SquareDigit.java
@@ -1,10 +1,13 @@
 package squareDigit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SquareDigit {
     public static void main(String[] args) {
         Integer m = 123;
         String numbers = String.valueOf(m);
         String[] digits2 = numbers.split("");
-
+        List<String> digits3 = new ArrayList<>();
     }
 }
