c4fba3e1cc76f2ac26ad2f10daddbd17ce69736b
MichalGuz <michalguz@gmail.com>
2020-02-14 05:09:11 +0100

    Create empty ArrayList and declare String variable inside Valentines class."

--- src/main/java/com/stream/valent/Valentines.java
+++ src/main/java/com/stream/valent/Valentines.java
@@ -1,9 +1,15 @@
 package com.stream.valent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Valentines {
     public static void main(String[] args) {
         String mess = "chaosonicokchaosuoychaosevolchaosi";
         String[] splitted = mess.split("chaos");
+
+        List<String> revealedTruth = new ArrayList<>();
+        String element;
 
     }
 }
