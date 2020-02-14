f699c80ded6dbc6e3b1e2c13902913ae12c9ad9d
MichalGuz <michalguz@gmail.com>
2020-02-14 05:07:52 +0100

    Split the String 'mess' into array.

--- src/main/java/com/stream/valent/Valentines.java
+++ src/main/java/com/stream/valent/Valentines.java
@@ -3,5 +3,7 @@ package com.stream.valent;
 public class Valentines {
     public static void main(String[] args) {
         String mess = "chaosonicokchaosuoychaosevolchaosi";
+        String[] splitted = mess.split("chaos");
+
     }
 }
