cfef016527cf6b0cc91f5977cd5100f73cccdc23
MichalGuz <michalguz@gmail.com>
2020-02-15 19:54:46 +0100

    Println() value of Integer variable prior created inside class SquareDigitsStream.

--- src/main/java/com/stream/squared_digits_stream/SquareDigitsStream.java
+++ src/main/java/com/stream/squared_digits_stream/SquareDigitsStream.java
@@ -3,5 +3,6 @@ package com.stream.squared_digits_stream;
 public class SquareDigitsStream {
      public static void main(String[] args) {
          Integer bigNumber = 54321;
+         System.out.println("Basic numbers: " + bigNumber);
      }
 }
