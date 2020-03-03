31fd1a1eb717c51845d5bfaf667673a3709b877d
MichalGuz <michalguz@gmail.com>
2020-03-03 23:53:59 +0100

    Create if condition inside body of static fibonacciFoundNumber(int n) method.

--- src/main/java/com/fibonacci_recursion/FibRecursion.java
+++ src/main/java/com/fibonacci_recursion/FibRecursion.java
@@ -2,5 +2,8 @@ package com.fibonacci_recursion;
 
 public class FibRecursion {
     public static int fibonacciFoundNumber(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
     }
 }
