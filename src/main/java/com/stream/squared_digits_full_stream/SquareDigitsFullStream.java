0eb354b590835c5729bec69353175eb266aa7970
MichalGuz <michalguz@gmail.com>
2020-04-13 17:36:06 +0200

    Map() the stream of chars by calling parseInt() and valueOf() methods inside the body of squareDigitsUsingStream(int n) method.

--- src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStream.java
+++ src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStream.java
@@ -4,6 +4,8 @@ public class SquareDigitsFullStream {
     public int squareDigitsUsingStream(int n){
         return Integer.parseInt(String.valueOf(n)
         .chars()
+                .map(i -> Integer.parseInt(String.valueOf((char) i)))
+                .
         );
     }
 }
