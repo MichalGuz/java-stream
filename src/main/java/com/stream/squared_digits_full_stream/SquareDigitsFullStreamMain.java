737c815d0606751cac524000ef881863624b567e
MichalGuz <michalguz@gmail.com>
2020-04-13 18:44:09 +0200

    Create new obeject of SquareDigitsFullStream class and call squareDigitsUsingStream() method.

--- src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStreamMain.java
+++ src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStreamMain.java
@@ -2,6 +2,7 @@ package com.stream.squared_digits_full_stream;
 
 public class SquareDigitsFullStreamMain {
     public static void main(String[] args) {
-
+        SquareDigitsFullStream s = new SquareDigitsFullStream();
+        int result = s.squareDigitsUsingStream(123405);
     }
 }
