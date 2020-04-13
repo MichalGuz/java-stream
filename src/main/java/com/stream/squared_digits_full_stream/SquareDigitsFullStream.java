4b8b8e85037aabefe7f3cc905c6c1714bdb702bd
MichalGuz <michalguz@gmail.com>
2020-04-13 17:23:46 +0200

    Add return statement in method squareDigitsUsingStream(int n)and call Integer parseInt()and String valueOf() methods.

--- src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStream.java
+++ src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStream.java
@@ -2,6 +2,6 @@ package com.stream.squared_digits_full_stream;
 
 public class SquareDigitsFullStream {
     public int squareDigitsUsingStream(int n){
-
+        return Integer.parseInt(String.valueOf(n));
     }
 }
