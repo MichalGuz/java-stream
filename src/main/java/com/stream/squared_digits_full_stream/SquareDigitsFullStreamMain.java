bd2bef24bb3de660889ecbef9ea51956123840c7
MichalGuz <michalguz@gmail.com>
2020-04-13 18:45:15 +0200

    Println() squared digits.

--- src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStreamMain.java
+++ src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStreamMain.java
@@ -4,5 +4,6 @@ public class SquareDigitsFullStreamMain {
     public static void main(String[] args) {
         SquareDigitsFullStream s = new SquareDigitsFullStream();
         int result = s.squareDigitsUsingStream(123405);
+        System.out.println("Squared digits = " + result);
     }
 }
