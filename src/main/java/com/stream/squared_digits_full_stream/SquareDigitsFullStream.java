1b948c68baa9d535a59262d05d845dc3613f3548
MichalGuz <michalguz@gmail.com>
2020-04-13 17:46:36 +0200

    Call collect(Collectors.joinning()) method to end stream with a single String object inside the body of squareDigitsUsingStream(int n) method.

--- src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStream.java
+++ src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStream.java
@@ -1,5 +1,7 @@
 package com.stream.squared_digits_full_stream;
 
+import java.util.stream.Collectors;
+
 public class SquareDigitsFullStream {
     public int squareDigitsUsingStream(int n){
         return Integer.parseInt(String.valueOf(n)
@@ -7,7 +9,7 @@ public class SquareDigitsFullStream {
                 .map(i -> Integer.parseInt(String.valueOf((char) i)))
                 .map(i -> i * i)
                 .mapToObj(String::valueOf)
-                .
+                .collect(Collectors.joining())
         );
     }
 }
