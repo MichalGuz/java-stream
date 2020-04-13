af7ed86662a526ea4df22ffb1276e46e0c000c3a
MichalGuz <michalguz@gmail.com>
2020-04-13 18:39:58 +0200

    Clear the spaces in the code.

--- src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStream.java
+++ src/main/java/com/stream/squared_digits_full_stream/SquareDigitsFullStream.java
@@ -5,11 +5,10 @@ import java.util.stream.Collectors;
 public class SquareDigitsFullStream {
     public int squareDigitsUsingStream(int n){
         return Integer.parseInt(String.valueOf(n)
-        .chars()
+                .chars()
                 .map(i -> Integer.parseInt(String.valueOf((char) i)))
                 .map(i -> i * i)
                 .mapToObj(String::valueOf)
-                .collect(Collectors.joining())
-        );
+                .collect(Collectors.joining()));
     }
 }
