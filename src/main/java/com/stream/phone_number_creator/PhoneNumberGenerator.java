1f4cf190754fca7feaa8bc637234eaf1037a3fb7
MichalGuz <michalguz@gmail.com>
2020-04-22 22:22:37 +0200

    Create 'if' condition in generatePhoneNumber(int[] n) method to verify proper lenght of array.

--- src/main/java/com/stream/phone_number_creator/PhoneNumberGenerator.java
+++ src/main/java/com/stream/phone_number_creator/PhoneNumberGenerator.java
@@ -2,6 +2,7 @@ package com.stream.phone_number_creator;
 
 public class PhoneNumberGenerator {
     public String generatePhoneNumber(int[] n) {
+        if (n.length != 9) throw new IllegalArgumentException("Array must contain 9 integers!");
 
     }
 }
