adece25f9cb6c5738edbf8b135135faedede1af1
MichalGuz <michalguz@gmail.com>
2020-04-22 22:26:05 +0200

    Create loop 'for each' in generatePhoneNumber(int[] n) method.

--- src/main/java/com/stream/phone_number_creator/PhoneNumberGenerator.java
+++ src/main/java/com/stream/phone_number_creator/PhoneNumberGenerator.java
@@ -3,6 +3,8 @@ package com.stream.phone_number_creator;
 public class PhoneNumberGenerator {
     public String generatePhoneNumber(int[] n) {
         if (n.length != 9) throw new IllegalArgumentException("Array must contain 9 integers!");
+        for (int i: n){
 
+        }
     }
 }
