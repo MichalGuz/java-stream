52784d6d91ddbf2441233e2dac86d17fe25a29e1
MichalGuz <michalguz@gmail.com>
2020-04-22 22:34:34 +0200

    Call String.format() method with using of IntStream.of() and toArray() methods in generatePhoneNumber(int[] n) method.

--- src/main/java/com/stream/phone_number_creator/PhoneNumberGenerator.java
+++ src/main/java/com/stream/phone_number_creator/PhoneNumberGenerator.java
@@ -1,10 +1,13 @@
 package com.stream.phone_number_creator;
 
+import java.util.stream.IntStream;
+
 public class PhoneNumberGenerator {
     public String generatePhoneNumber(int[] n) {
         if (n.length != 9) throw new IllegalArgumentException("Array must contain 9 integers!");
         for (int i: n){
             if (n[i]<0 || n[i]>9) throw new IllegalArgumentException("Array must contain only integers between 0-9.");
         }
+        String.format("(%d%d%d) %d%d%d-%d%d%d", IntStream.of(n).toArray());
     }
 }
