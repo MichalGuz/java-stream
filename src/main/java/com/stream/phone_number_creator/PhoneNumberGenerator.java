105698da650b4796c5227f8bd7d2acaaab4268cb
MichalGuz <michalguz@gmail.com>
2020-04-22 22:29:45 +0200

    Create 'if' condition in generatePhoneNumber(int[] n) method to verify if array contains only single digits.

--- src/main/java/com/stream/phone_number_creator/PhoneNumberGenerator.java
+++ src/main/java/com/stream/phone_number_creator/PhoneNumberGenerator.java
@@ -4,7 +4,7 @@ public class PhoneNumberGenerator {
     public String generatePhoneNumber(int[] n) {
         if (n.length != 9) throw new IllegalArgumentException("Array must contain 9 integers!");
         for (int i: n){
-
+            if (n[i]<0 || n[i]>9) throw new IllegalArgumentException("Array must contain only integers between 0-9.");
         }
     }
 }
