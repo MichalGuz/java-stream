c0611112300cf5876103ea1079f4d6f601ea97f4
MichalGuz <michalguz@gmail.com>
2020-05-03 23:15:25 +0200

    Add in latest map() method calls substring() and toLowerCase() methods - inside accumWithStream() method.

--- src/main/java/com/stream/mumbled_letters/MumbledLettersStream.java
+++ src/main/java/com/stream/mumbled_letters/MumbledLettersStream.java
@@ -7,7 +7,7 @@ public class MumbledLettersStream {
     public static String accumWithStream(String s) {
         return IntStream.range(0, s.length())
                 .mapToObj(i -> Stream.generate(() -> s.charAt(i)).limit(i+1).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString())
-                .map(j -> j.substring(0,1).toUpperCase())
+                .map(j -> j.substring(0,1).toUpperCase() + j.substring(1).toLowerCase())
 
     }
 }
