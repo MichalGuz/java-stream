b64653641aabcc5754befb9fa64be82dd8b9bbed
MichalGuz <michalguz@gmail.com>
2020-05-03 22:04:46 +0200

    Map stream of ints using method mapToObj() and next generate() infinite stream of Characters retrieved from 's' String with charAt() methid inside accumWithStream() method.

--- src/main/java/com/stream/mumbled_letters/MumbledLettersStream.java
+++ src/main/java/com/stream/mumbled_letters/MumbledLettersStream.java
@@ -1,10 +1,12 @@
 package com.stream.mumbled_letters;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MumbledLettersStream {
     public static String accumWithStream(String s) {
         return IntStream.range(0, s.length())
+                .mapToObj(i -> Stream.generate(() -> s.charAt(i)))
 
     }
 }
