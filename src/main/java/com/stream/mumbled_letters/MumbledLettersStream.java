9633a45451b023eaef6d51b5adf849214ac16a72
MichalGuz <michalguz@gmail.com>
2020-05-03 21:36:25 +0200

    Initialize stream of ints using IntStream.range() inside accumWithStream() method.

--- src/main/java/com/stream/mumbled_letters/MumbledLettersStream.java
+++ src/main/java/com/stream/mumbled_letters/MumbledLettersStream.java
@@ -1,7 +1,10 @@
 package com.stream.mumbled_letters;
 
+import java.util.stream.IntStream;
+
 public class MumbledLettersStream {
     public static String accumWithStream(String s) {
+        return IntStream.range(0, s.length())
 
     }
 }
