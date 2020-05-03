81e25ab34e87481e9943d4ef5c6bf4920f35879b
MichalGuz <michalguz@gmail.com>
2020-05-03 23:17:31 +0200

    End the stream of Strings by calling of collect() method with Collectors.joining() the '-' delimiter - inside accumWithStream() method.

--- src/main/java/com/stream/mumbled_letters/MumbledLettersStream.java
+++ src/main/java/com/stream/mumbled_letters/MumbledLettersStream.java
@@ -1,5 +1,6 @@
 package com.stream.mumbled_letters;
 
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -8,6 +9,7 @@ public class MumbledLettersStream {
         return IntStream.range(0, s.length())
                 .mapToObj(i -> Stream.generate(() -> s.charAt(i)).limit(i+1).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString())
                 .map(j -> j.substring(0,1).toUpperCase() + j.substring(1).toLowerCase())
+                .collect(Collectors.joining("-"));
 
     }
 }
